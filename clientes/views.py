7c886286137823479b9f75322929dfdd08052f1a
dnetochaves <dnetochaves@gmail.com>
2020-03-12 07:04:21 -0300

    login and App website implementation

--- clientes/views.py
+++ clientes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-#@login_required
+@login_required
 def start(request):
     #return HttpResponse('start')
     return render(request, 'clientes/start_clientes.html')
--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from clientes import urls as clientes_urls
+from website import urls as website_urls
+from django.contrib.auth.views import LoginView, LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include(clientes_urls)),
+    path('', include(website_urls)),
+    path('login/', LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]

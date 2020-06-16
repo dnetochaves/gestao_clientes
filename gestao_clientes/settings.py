05ce8ca8cb5a63a29621a9e6c39fd72c87b7d3ff
dnetochaves <dnetochaves@gmail.com>
2020-06-16 09:57:07 -0300

    Configuraçao Settings

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -145,11 +145,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    'static',
-]
+#STATICFILES_DIRS = [
+#    'static',
+#]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")

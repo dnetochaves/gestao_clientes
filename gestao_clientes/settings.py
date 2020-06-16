04d558d6422a0fe7e99ac6fffff8ae54551e4cbd
dnetochaves <dnetochaves@gmail.com>
2020-06-16 09:24:26 -0300

    Alteraçao arquivo settings

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -145,9 +145,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    'static',
-]
+#STATICFILES_DIRS = [
+#    'static',
+#]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
